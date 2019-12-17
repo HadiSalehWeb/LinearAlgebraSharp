164a318a7a661ac8f65f0acf09122bf3c2192048
Hadi Saleh <hadi.saleh852@outlook.com>
2019-12-17 14:40:25 +0100

    Added extension methods for IEnumerables on Scalars

---
+++ LinearAlgebraSharp/Scalars/ScalarEnumerable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LinearAlgebraSharp.Scalars
+{
+    public static class ScalarEnumerable
+    {
+        public static Scalar<T> Sum<T>(this IEnumerable<Scalar<T>> list)
+            where T : struct
+        {
+            Scalar<T> res = Scalar<T>.Zero;
+
+            foreach (var sca in list)
+                res += sca;
+
+            return res;
+        }
+
+        public static Scalar<T> Average<T>(this IEnumerable<Scalar<T>> list)
+            where T : struct
+        {
+            return list.Sum() / list.Count();
+        }
+
+        public static Scalar<T> Max<T>(this IEnumerable<Scalar<T>> list)
+            where T : struct
+        {
+            var max = Scalar<T>.MinValue;
+
+            foreach (var item in list)
+                if (item > max)
+                    max = item;
+
+            return max;
+        }
+
+        public static Scalar<T> Min<T>(this IEnumerable<Scalar<T>> list)
+            where T : struct
+        {
+            var min = Scalar<T>.MaxValue;
+
+            foreach (var item in list)
+                if (item < min)
+                    min = item;
+
+            return min;
+        }
+    }
+}
